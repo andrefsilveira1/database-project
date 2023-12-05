b60dd00e27ce1c981229094d90a79f8572d2a161
Minobarbarian <tonho_kun@ymail.com>
2023-12-05 15:47:38 -0300

    inserts de entrance

--- sql/querys.sql
+++ sql/querys.sql
@@ -36,10 +36,27 @@ VALUES (1, 1, CURRENT_TIMESTAMP),
        (3, 8, CURRENT_TIMESTAMP);
 
 INSERT INTO Speaker (event_member_id, specialization)
-VALUES (1, 'Specialization1');
+VALUES (5, 'Specialization1'),
+       (10, 'Specialization2'),
+       (11, 'Specialization3');
 
 INSERT INTO Entrance (event_id, member_id, role, price, purchased_at)
-VALUES (1, 1, 'Role1', 25.99, '2023-11-17 12:04:00');
+VALUES (1, 1, 'Role1', 25.99, CURRENT_TIMESTAMP),
+       (1, 3, 'Role2', 25.99, CURRENT_TIMESTAMP),
+       (1, 4, 'Role3', 25.99, CURRENT_TIMESTAMP),
+       (1, 7, 'Role4', 25.99, CURRENT_TIMESTAMP),
+       (1, 8, 'Role5', 25.99, CURRENT_TIMESTAMP),
+       (2, 1, 'Role6', 25.99, CURRENT_TIMESTAMP),
+       (2, 2, 'Role7', 25.99, CURRENT_TIMESTAMP),
+       (2, 5, 'Role8', 25.99, CURRENT_TIMESTAMP),
+       (2, 6, 'Role9', 25.99, CURRENT_TIMESTAMP),
+       (2, 7, 'Role10', 25.99, CURRENT_TIMESTAMP),
+       (3, 2, 'Role11', 25.99, CURRENT_TIMESTAMP),
+       (3, 3, 'Role12', 25.99, CURRENT_TIMESTAMP),
+       (3, 4, 'Role13', 25.99, CURRENT_TIMESTAMP),
+       (3, 5, 'Role14', 25.99, CURRENT_TIMESTAMP),
+       (3, 6, 'Role15', 25.99, CURRENT_TIMESTAMP),
+       (3, 8, 'Role16', 25.99, CURRENT_TIMESTAMP);
 
 
 
