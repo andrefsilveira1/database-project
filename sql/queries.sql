1dec10c95fafbe100be32eea8be105bc2239f9e4
Minobarbarian <tonho_kun@ymail.com>
2023-12-05 17:29:23 -0300

    Filtro, União, Ordenação , Subconsulta

--- sql/queries.sql
+++ sql/queries.sql
@@ -10,22 +10,44 @@ SELECT e.name AS 'event_name',
        e.date AS 'event_date'
 FROM Event e JOIN
      Place p
-     ON (e.place_id =
-         p.id);
+     ON (e.place_id = p.id);
 
 -- Consulta de Agregação:
 
-SELECT SUM(price) AS
-FROM ENTRANCE
+SELECT SUM(price) AS 'event3_sum'
+FROM Entrance
 WHERE event_id = 3;
 
--- Filter Query:
+-- Consulta de Filtro:
 
--- Sort Query:
+SELECT name AS 'member_name'
+FROM Member
+WHERE CAST(strftime('%Y', born) AS INTEGER) < 2000;
 
--- Subquery Query:
+-- Consulta de Ordenação:
 
--- Join Query:
+SELECT id, role, price
+FROM Entrance
+ORDER BY price DESC, role;
+
+-- Consulta de Subconsulta:
+
+SELECT m.name AS 'member_name'
+FROM Member m
+WHERE m.id
+(SELECT em.member_id
+FROM EventMember em
+WHERE em.eventId = 1);
+
+-- Consulta de União:
+
+SELECT m.name AS 'member_name', m.born AS 'born_in'
+FROM Member m
+
+UNION
+
+SELECT p.name AS 'place_name'
+FROM Place p;
 
 -- Group Query:
 
