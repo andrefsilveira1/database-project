9c4b3aad884c6d23bc4aff5d32cdf4608403165a
Minobarbarian <tonho_kun@ymail.com>
2023-12-05 18:28:15 -0300

    Mais 9 Consultas (Uma de cada tipo, menos complex)

--- sql/queries.sql
+++ sql/queries.sql
@@ -3,6 +3,9 @@
 SELECT *
 FROM Entrance;
 
+SELECT *
+FROM Member;
+
 -- Consulta de Junção:
 
 SELECT e.name AS 'event_name',
@@ -13,36 +16,69 @@ FROM Event AS e
      Place AS p
      ON (e.place_id = p.id);
 
+SELECT e.name AS 'event_name',
+       s.specialization AS 'Speaker' 
+FROM Event AS e 
+     JOIN
+     EventMember AS em
+     ON (e.id = em.eventId)
+     JOIN
+     Speaker s
+     ON (s.event_member_id = em.id);
+
 -- Consulta de Agregação:
 
 SELECT SUM(price) AS 'event3_sum'
 FROM Entrance
 WHERE event_id = 3;
 
+SELECT COUNT(event_id) AS 'event2_amount'
+FROM Entrance
+WHERE price = 13.99
+
 -- Consulta com Filtro:
 
-SELECT name AS 'member_name'
+SELECT name AS 'members_born_before_2000'
 FROM Member
 WHERE CAST(strftime('%Y', born) AS INTEGER) < 2000;
 
+SELECT name AS 'member_born_after_2000'
+FROM Member
+WHERE CAST(strftime('%Y', born) AS INTEGER) > 2000;
+
 -- Consulta com Ordenação:
 
-SELECT id, role, price
+SELECT id,
+       role,
+       price
 FROM Entrance
 ORDER BY price DESC, role;
 
+SELECT name AS 'member_name'
+       strftime('%Y', born) AS 'birth_year'
+FROM Member
+ORDER BY CAST(strftime('%Y', born) AS INTEGER);
+
 -- Consulta com Subconsulta:
 
-SELECT m.name AS 'member_name'
+SELECT m.name AS 'event1_members'
 FROM Member AS m
 WHERE m.id
 (SELECT em.member_id
 FROM EventMember AS em
 WHERE em.eventId = 1);
 
+SELECT e.name AS 'place1_events'
+FROM Event AS e
+WHERE e.place_id
+(SELECT p.id
+FROM Place AS p
+WHERE p.id = 1);
+
 -- Consulta com União:
 
-SELECT m.name AS 'member_name', m.born AS 'born_in'
+SELECT m.name AS 'member_name',
+       m.born AS 'born_in'
 FROM Member AS m
 
 UNION
@@ -50,10 +86,24 @@ UNION
 SELECT p.name AS 'place_name'
 FROM Place AS p;
 
+SELECT s.specialization AS 'speaker_specialization'
+FROM Speaker AS s
+
+UNION
+
+SELECT e.role AS 'entrance_role'
+FROM Entrance AS e;
+
 -- Consulta com Agroupamento:
 
-SELECT event_id, SUM(price) AS 'sum'
+SELECT event_id,
+       SUM(price) AS 'sum'
 FROM Entrance
+GROUP BY event_id;
+
+SELECT event_id,
+       COUNT(member_id) AS 'members_amount'
+FROM EventMember
 GROUP BY event_id;
 
 -- Consulta com Limite:
@@ -64,7 +114,9 @@ LIMIT 3;
 
 -- Consulta Complexa:
 
-SELECT m.id, m.name, s.specialization
+SELECT m.id,
+       m.name AS 'member_name',
+       s.specialization AS 'member_specialization'
 FROM Member AS m
      JOIN
      EventMember AS em
